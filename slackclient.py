f94e99d63e990413e4337a4f71d596939ca5082d
Sandy CHAN <sandydy.chan@gmail.com>
2021-10-13 00:18:41 -0300

    fix return message error

--- slackclient.py
+++ slackclient.py
@@ -9,11 +9,13 @@ def slack_logger(event, context):
     from slack.errors import SlackApiError
     slack_token = os.environ["SLACK_API_TOKEN"]
     client = WebClient(token=slack_token)
+    message = json.dumps(event)
+    channel = os.environ["SLACK_API_CHANNEL"]
 
     try:
       response = client.chat_postMessage(
-        channel=os.environ["SLACK_API_CHANNEL"],
-        text=json.dumps(event)
+        channel=channel,
+        text= message
       )
     except SlackApiError as e:
       # You will get a SlackApiError if "ok" is False
@@ -24,5 +26,5 @@ def slack_logger(event, context):
     
     return {
         'statusCode': 200,
-        'body': json.dumps("message '"+ event["message"] + "' sent to channel: '" + event["channel"] + "'")
+        'body': json.dumps("message '"+ message + "' sent to channel: '" + channel + "'")
     }
